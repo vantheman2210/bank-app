99cedfea791f9e0ef40c0b2a4c263ea47f400a4c
vantheman2210 <martinovic.ba@gmail.com>
2023-04-18 10:04:57 +0200

    creating function that creates usernames

--- script.js
+++ script.js
@@ -6,34 +6,34 @@
 
 // Data
 const account1 = {
-  owner: 'Vanja Martinovic',
-  movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
-  interestRate: 1.2, // %
-  pin: 1111,
+	owner: 'Vanja Martinovic',
+	movements: [ 200, 450, -400, 3000, -650, -130, 70, 1300 ],
+	interestRate: 1.2, // %
+	pin: 1111
 };
 
 const account2 = {
-  owner: 'Jessica Davis',
-  movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
-  interestRate: 1.5,
-  pin: 2222,
+	owner: 'Jessica Davis',
+	movements: [ 5000, 3400, -150, -790, -3210, -1000, 8500, -30 ],
+	interestRate: 1.5,
+	pin: 2222
 };
 
 const account3 = {
-  owner: 'Steven Thomas Williams',
-  movements: [200, -200, 340, -300, -20, 50, 400, -460],
-  interestRate: 0.7,
-  pin: 3333,
+	owner: 'Steven Thomas Williams',
+	movements: [ 200, -200, 340, -300, -20, 50, 400, -460 ],
+	interestRate: 0.7,
+	pin: 3333
 };
 
 const account4 = {
-  owner: 'Sarah Smith',
-  movements: [430, 1000, 700, 50, 90],
-  interestRate: 1,
-  pin: 4444,
+	owner: 'Sarah Smith',
+	movements: [ 430, 1000, 700, 50, 90 ],
+	interestRate: 1,
+	pin: 4444
 };
 
-const accounts = [account1, account2, account3, account4];
+const accounts = [ account1, account2, account3, account4 ];
 
 // Elements
 const labelWelcome = document.querySelector('.welcome');
@@ -62,20 +62,26 @@ const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
 // Displaying movements from and to account
-const displayMovements = function (movements) { 
-  // Same as setting values .textContent = 0
-  containerMovements.innerHTML = '';
+const displayMovements = function(movements) {
+	// Same as setting values .textContent = 0
+	containerMovements.innerHTML = '';
 
-  movements.forEach((mov, i) => { 
-    const type = mov > 0 ? 'deposit' : 'withdrawal'
+	movements.forEach((mov, i) => {
+		const type = mov > 0 ? 'deposit' : 'withdrawal';
 
-    const html = `
+		const html = `
     <div class="movements__row">
     <div class="movements__type movements__type--${type}">${i + 1} ${type}</div>
     <div class="movements__value">${mov}€</div>
   </div>
     `;
-    containerMovements.insertAdjacentHTML('afterbegin', html);
-  })
+		containerMovements.insertAdjacentHTML('afterbegin', html);
+	});
 };
 displayMovements(account1.movements);
+
+const createUsernames = function(user) { 
+  const username = user.toLowerCase().split(' ').map((letter) => letter[0]).join('');
+  return username;
+}
+
